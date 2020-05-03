099df2399d7c5d72e42319445bb0e2512f5fd8fd
Clayton <clayton.weller@gmail.com>
2020-05-03 00:24:44 -0600

    Next module action works

--- src/actions/modules/index.js
+++ src/actions/modules/index.js
@@ -2,4 +2,17 @@ import { preshowActionHash } from './preshow';
 
 export const moduleActionHash = {
   ...preshowActionHash,
+  'start-next-module': startNextModuleAction,
 };
+
+function startNextModuleAction(params, component) {
+  const { currentModule, performance } = params;
+
+  console.log('Starting module:', currentModule.module.title);
+
+  component.setState({
+    moduleState: {},
+    currentModule,
+    performance,
+  });
+}
