a04d6432394a8892cbba5bb9e7a58f8fbaeb52f4
Clayton <clayton.weller@gmail.com>
2020-05-19 08:58:10 -0600

    Starting to name boats

--- src/actions/modules/boatrace.js
+++ src/actions/modules/boatrace.js
@@ -2,6 +2,7 @@ export const boatraceActionHash = {
   'boatrace-show-title': showTitleAction,
   'boatrace-ready-to-board': readyToBoardAction,
   'boatrace-boat-boarded': boatBoardedAction,
+  'boatrace-coxswains-selected': coxswainsSelectedAction,
 };
 
 function showTitleAction(params, component) {
@@ -28,6 +29,15 @@ function boatBoardedAction(params, component) {
       step: 'boarding',
       boats: params.allBoats,
       lastBoarded: params.boat,
+    },
+  });
+}
+
+function coxswainsSelectedAction(params, component) {
+  component.setState({
+    moduleState: {
+      step: 'boarding',
+      ...params,
     },
   });
 }
